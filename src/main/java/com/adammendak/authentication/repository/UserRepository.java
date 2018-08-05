100ba1e3554cfa3d6c1c7ff074a72945b00328e9
adammendak <adam.mendak@gmail.com>
2018-08-05 18:55:52 +0200

    more

--- src/main/java/com/adammendak/authentication/repository/UserRepository.java
+++ src/main/java/com/adammendak/authentication/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.adammendak.authentication.repository;
 
 import com.adammendak.authentication.model.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 }
